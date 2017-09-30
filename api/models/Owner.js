d6abea74e06b7c456089bd42228586eae2cd6b73
DJCrossman <david@crossman.io>
2017-09-29 20:52:50 -0600

    Added api

---
+++ api/models/Owner.js
@@ -0,0 +1,74 @@
+/**
+ * Owner.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    id: {
+      type: 'string',
+      unique: true,
+      primaryKey: true,
+      defaultsTo: function() {
+        function s4() {
+          return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+      }
+    },
+    firstName: {
+      type: 'string',
+      required: true
+    },
+    lastName: {
+      type: 'string',
+      required: true
+    },
+    birthdate: {
+      type: 'date',
+      required: true
+    },
+    address: {
+      type: 'address',
+      required: true
+    },
+    occupation: {
+      type: 'string'
+    },
+    schedule: {
+      type: 'string',
+      enum: ['Very Busy', 'Busy', 'Not Busy']
+    },
+    pets: {
+      type: 'array'
+    },
+    hasFostered: {
+      type: 'boolean',
+      required: true
+    },
+    fosterExperience: {
+      type: 'string'
+    },
+    agePreference: {
+      type: 'array'
+    },
+    hoursAlone: {
+      type: 'float'
+    },
+    hoursExercising: {
+      type: 'float'
+    },
+    periodicVisit: {
+      type: 'boolean'
+    },
+    hesitations: {
+      type: 'string'
+    },
+    campaign: {
+      type: 'string'
+    }
+  }
+};
---
+++ api/models/Pet.js
@@ -0,0 +1,66 @@
+/**
+ * Pet.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    id: {
+      type: 'string',
+      unique: true,
+      primaryKey: true,
+      defaultsTo: function() {
+        function s4() {
+          return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+      }
+    },
+    name: {
+      type: 'string'
+    },
+    state: {
+      type: 'string',
+      enum: ['pending', 'approved', 'denied', 'adopted', 'fostered'],
+      required: true
+    },
+    species: {
+      type: 'string',
+      required: true
+    },
+    breed: {
+      type: 'string',
+      required: true
+    },
+    traits: {
+      type: 'string'
+    },
+    birthdate: {
+      type: 'date',
+      required: true
+    },
+    sex: {
+      type: 'string',
+      enum: ['male', 'female', 'other'],
+      required: true
+    },
+    diet: {
+      type: 'string'
+    },
+    tatoo: {
+      type: 'string'
+    },
+    microchip: {
+      type: 'string'
+    }
+    photo: {
+      type: 'boolean'
+    },
+    description: {
+      type: 'string'
+    }
+  }
+};
